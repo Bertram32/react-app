34e5b11641542355f8b0a802132a0924b121c728
Bertram32 <buzuu@outlook.dk>
2024-12-15 16:09:15 +0100

    v1.0.1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <section className="section1"></section>
+      <section className="section1" id="section1">
+        <p className="head">Ber3</p>
+        <menu className="menu">
+          <li>
+            <a href="#section1">Home</a>
+          </li>
+          <li>
+            <a href="#section2">Contact</a>
+          </li>
+          <li>
+            <a href="#section3">About</a>
+          </li>
+        </menu>
+      </section>
+      <section className="section2" id="section2"></section>
 
-      <section className="section2"></section>
-
-      <section className="section3"></section>
+      <section className="section3" id="section3"></section>
     </div>
   );
 }
