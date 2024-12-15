16f65a65b10f6119c2c415562c2fe8ce917399a3
Bertram32 <buzuu@outlook.dk>
2024-12-15 14:57:54 +0100

    Update: New look

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="section1"></section>
+
+      <section className="section2"></section>
+
+      <section className="section3"></section>
+    </div>
+  );
+}
+
+export default App;
